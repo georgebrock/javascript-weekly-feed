515d1da45dcafee054dd98ef45a389f9775051e9
George Brocklehurst <george.brocklehurst@gmail.com>
2011-05-02 03:50:21 +0100

    Functional atom feed.

--- javascript_weekly_to_rss.rb
+++ javascript_weekly_to_rss.rb
@@ -2,25 +2,57 @@ require 'sinatra/base'
 require 'erb'
 require 'nokogiri'
 require 'open-uri'
+require 'net/http'
+require 'cgi'
 
 module JavascriptWeekly
   BASE_URL = 'http://javascriptweekly.com/archive'
 
-  def latest_issue
+  def latest_issue_number
     @latest_issue ||= (
       doc = Nokogiri::HTML(open(BASE_URL))
       doc.css('a').map{|l| l.text.to_i }.max
     )
   end
 
-  def issue_url(number)
-    '%s/%d.html' % [BASE_URL, number]
-  end
-
   def issue(number)
+    @issues ||= {}
+    @issues[number] ||= JavascriptWeekly::Issue.new(number)
   end
 
   extend self
+end
+
+class JavascriptWeekly::Issue
+  attr_reader :number
+
+  def initialize(number)
+    @number = number
+    @last_modified, @content = nil, nil
+  end
+
+  def url
+    '%s/%d.html' % [JavascriptWeekly::BASE_URL, number]
+  end
+
+  def content
+    load if @content.nil?
+    @content
+  end
+
+  def last_modified
+    load if @last_modified.nil?
+    @last_modified
+  end
+
+protected
+  def load
+    url = URI.parse(self.url)
+    response = Net::HTTP.start(url.host, url.port){|http| http.get(url.path) }
+    @content = Nokogiri::HTML::Document.parse(response.body).css('body').inner_html
+    @last_modified = response['Last-Modified']
+  end
+
 end
 
 class JavascriptWeeklyToRss < Sinatra::Base
@@ -35,24 +67,10 @@ class JavascriptWeeklyToRss < Sinatra::Base
   end
 
   helpers do
-    def latest_issue
-      JavascriptWeekly.latest_issue
-    end
-
-    def first_relevant_issue
-      latest_issue - 10
-    end
-
-    def issue_url(number)
-      JavascriptWeekly.issue_url(number)
-    end
-
-    def issue_publication_date(number)
-      1
-    end
-
-    def issue_content(number)
-      '...'
+    def issues
+      last_number = JavascriptWeekly.latest_issue_number
+      first_number = last_number - 10
+      (first_number..last_number).to_a.reverse.map{|n| JavascriptWeekly.issue(n) }
     end
   end
 
